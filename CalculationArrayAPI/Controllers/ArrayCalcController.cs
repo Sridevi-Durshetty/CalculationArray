3c2474a1dbe16c736ec98b3be0366eac064fbb1a
Sridevi Mandarapu <durshetty.sridevi@gmail.com>
2021-06-07 12:40:43 +1000

    added array calc controller with routes

---
+++ CalculationArrayAPI/Controllers/ArrayCalcController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CalculationArrayAPI.Controllers
+{
+    [RoutePrefix("api/arraycalc")]
+    public class ArrayCalcController : ApiController
+    {
+        [HttpGet]
+        [Route("reverse")]
+        public IHttpActionResult ArrayReverse([FromUri] string[] productIds)
+        {
+            return Ok(productIds);
+        }
+
+        [HttpGet]
+        [Route("deletepart")]
+        public IHttpActionResult DeletePart([FromUri] string position, [FromUri] string[] productIds)
+        {           
+             return Ok(productIds);
+        }
+    }
+}
