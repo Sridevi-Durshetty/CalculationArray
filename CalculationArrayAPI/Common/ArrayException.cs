3e3031ca256efc2ce5e312f326f9e62dc596e919
Sridevi Mandarapu <durshetty.sridevi@gmail.com>
2021-06-07 12:46:02 +1000

    added common exception class for array

---
+++ CalculationArrayAPI/Common/ArrayException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace CalculationArrayAPI.Common
+{
+    public class ArrayException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public ArrayException(HttpStatusCode statusCode, string errorDescription) : base(errorDescription)
+        {
+            StatusCode = statusCode;
+        }
+    }
+
+    public class ArrayBadException : ArrayException
+    {
+        public ArrayBadException(string errorDescription) : base(HttpStatusCode.BadRequest, errorDescription)
+        {
+        }
+    }
+}
