c1c45dbf2c78ac7ad74ad1c2e75b418865385870
Sridevi Mandarapu <durshetty.sridevi@gmail.com>
2021-06-07 14:17:19 +1000

    basic test cases added

--- CalculationArrayAPI.Tests/ArrayCalcControllerTest.cs
+++ CalculationArrayAPI.Tests/ArrayCalcControllerTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Web.Http.Results;
+using CalculationArrayAPI.Common;
+using CalculationArrayAPI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CalculationArrayAPI.Tests
@@ -7,8 +11,44 @@ namespace CalculationArrayAPI.Tests
     public class ArrayCalcControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+       // [ExpectedException(typeof(ArrayBadException))]
+        public void ReverseArray__InputNullProductIds_ThrowBadRequest()
         {
+            string[] inputLst = null;
+            var controller = new ArrayCalcController();
+            var ex = Assert.ThrowsException<ArrayBadException>(() => controller.ArrayReverse(inputLst));
+            Assert.AreEqual("Please provide productsIds", ex.Message);
+        }
+
+        [TestMethod]       
+        public void Deletepart__InputNullProductIds_ThrowBadRequest()
+        {
+            string[] inputLst = null;
+            string position = "1";
+            var controller = new ArrayCalcController();
+            var ex = Assert.ThrowsException<ArrayBadException>(() => controller.DeletePart(position, inputLst));
+            Assert.AreEqual("Please provide productsIds and position", ex.Message);
+        }
+
+        [TestMethod]
+        public void Deletepart__InputNullPosition_ThrowBadRequest()
+        {
+            string[] inputLst = new[] { "1", "2", "3", "4", "5", "6" };
+            string position = null;
+            var controller = new ArrayCalcController();
+            var ex = Assert.ThrowsException<ArrayBadException>(() => controller.DeletePart(position, inputLst));
+            Assert.AreEqual("Please provide productsIds and position", ex.Message);
+        }
+
+
+        [TestMethod]
+        public void Deletepart__InputNullPositionProductIds_ThrowBadRequest()
+        {
+            string[] inputLst = null;
+            string position = null;
+            var controller = new ArrayCalcController();
+            var ex = Assert.ThrowsException<ArrayBadException>(() => controller.DeletePart(position, inputLst));
+            Assert.AreEqual("Please provide productsIds and position", ex.Message);
         }
     }
 }
