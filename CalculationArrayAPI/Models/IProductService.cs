60e1a4764df660b999d3f1d3dc492bc23b2a7345
Sridevi Mandarapu <durshetty.sridevi@gmail.com>
2021-06-07 12:20:46 +1000

    added models

---
+++ CalculationArrayAPI/Models/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculationArrayAPI.Models
+{
+    public interface IProductService
+    {
+        string[] ReverseArray(string[] prod);
+
+        string[] DeletePart(int position, string[] prod);
+    }
+}
